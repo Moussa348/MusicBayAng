d271eb95463176f2e35d2ecce496cca5cd996ca6
Moussa <Moussa.Keita2411@gmail.com>
2021-08-05 01:45:02 -0400

    adding homePage

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit, OnDestroy, NgZone, ChangeDetectorRef } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('fade', [
+      state('void', style({ opacity: 0 })),
+
+      transition(':enter, :leave', [animate(1000)]),
+    ]),
+  ]
 })
 export class HomeComponent implements OnInit {
 
